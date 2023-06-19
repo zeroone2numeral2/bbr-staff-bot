829c0a50288ab2b8ac37c68b30c7785f07941f7e
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-06-19 12:28:31 +0200

    aggiornato Chat in modo che supporti chat approvazioni (migrations) (#50)

---
+++ alembic/versions/8f74d7ec6b1a_added_chat_is_evaluation_chat.py
@@ -0,0 +1,24 @@
+"""added Chat.is_evaluation_chat
+
+Revision ID: 8f74d7ec6b1a
+Revises: a2401cf8abfd
+Create Date: 2023-06-19 12:25:34.232734
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8f74d7ec6b1a'
+down_revision = 'a2401cf8abfd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('chats', sa.Column('is_evaluation_chat', sa.Boolean))
+
+
+def downgrade() -> None:
+    op.drop_column('chats', 'is_evaluation_chat')
