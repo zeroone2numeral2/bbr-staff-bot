216ec0424a3973eee98d32db7c034b77e751bfb8
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-06-19 16:13:25 +0200

    /info: trigger per id (#70)

--- plugins/staff/chat/info.py
+++ plugins/staff/chat/info.py
@@ -1,11 +1,12 @@
 import logging
+import re
 
 from sqlalchemy.orm import Session
 from telegram import Update, helpers
 from telegram.ext import filters, PrefixHandler, ContextTypes
 
-from database.models import UserMessage, ChatMember as DbChatMember, Chat
-from database.queries import chats, user_messages, chat_members
+from database.models import UserMessage, ChatMember as DbChatMember, Chat, User
+from database.queries import chats, user_messages, chat_members, users
 import decorators
 import utilities
 from constants import COMMAND_PREFIXES, Group
@@ -19,49 +20,59 @@ logger = logging.getLogger(__name__)
 async def on_info_command(update: Update, context: ContextTypes.DEFAULT_TYPE, session: Session):
     logger.info(f"/info {utilities.log(update)}")
 
-    if not update.message.reply_to_message.from_user or update.message.reply_to_message.from_user.id == context.bot.id:
-        await update.effective_message.reply_text("Reply to an user's message")
-        return
+    if update.message.reply_to_message and update.message.reply_to_message.from_user.id == context.bot.id:
+        user_message: UserMessage = user_messages.get_user_message(session, update)
+        if not user_message:
+            logger.warning(f"couldn't find replied-to message, "
+                           f"chat_id: {update.effective_chat.id}; "
+                           f"message_id: {update.message.reply_to_message.message_id}")
+            await update.message.reply_text("no data saved for the replied-to message :(")
+            return
+        user: User = user_message.user
+    else:
+        user_id_match = re.search(r"(?:#user|#id)?(?P<user_id>\d+)", update.message.text, re.I)
+        if not user_id_match:
+            await update.message.reply_text("can't detect the user's id, reply to one of their forwarded message or include its id after the command")
+            return
 
-    user_message: UserMessage = user_messages.get_user_message(session, update)
-    if not user_message:
-        logger.warning(f"couldn't find replied-to message, "
-                       f"chat_id: {update.effective_chat.id}; "
-                       f"message_id: {update.message.reply_to_message.message_id}")
-        return
+        user_id = int(user_id_match.group("user_id"))
+        user: User = users.get_or_create(session, user_id, create_if_missing=False)
+        if not user:
+            await update.message.reply_text(f"can't find user <code>{user_id}</code> in the database")
+            return
 
-    text = f"• <b>name</b>: {helpers.mention_html(user_message.user.user_id, utilities.escape_html(user_message.user.name))}\n" \
-           f"• <b>username</b>: @{user_message.user.username or '-'}\n" \
-           f"• <b>first seen</b>: {user_message.user.first_seen}\n" \
-           f"• <b>last seen</b>: {user_message.user.last_message}\n" \
-           f"• <b>started</b>: {user_message.user.started} (on: {user_message.user.started_on})\n" \
-           f"• <b>stopped</b>: {user_message.user.stopped} (on: {user_message.user.stopped_on})\n" \
-           f"• <b>is bot/is premium</b>: {user_message.user.is_bot}, {user_message.user.is_premium}\n" \
-           f"• <b>language code (telegram)</b>: {user_message.user.language_code}\n" \
-           f"• <b>selected language</b>: {user_message.user.selected_language}"
+    text = f"• <b>name</b>: {user.mention()}\n" \
+           f"• <b>username</b>: @{user.username or '-'}\n" \
+           f"• <b>first seen</b>: {user.first_seen or '-'}\n" \
+           f"• <b>last seen</b>: {user.last_message or '-'}\n" \
+           f"• <b>started</b>: {user.started} (on: {user.started_on or '-'})\n" \
+           f"• <b>stopped</b>: {user.stopped} (on: {user.stopped_on or '-'})\n" \
+           f"• <b>is bot/is premium</b>: {user.is_bot}, {user.is_premium}\n" \
+           f"• <b>language code (telegram)</b>: {user.language_code or '-'}\n" \
+           f"• <b>selected language</b>: {user.selected_language or '-'}"
 
-    if user_message.user.banned:
-        text += f"\n• <b>banned</b>: {user_message.user.banned} (shadowban: {user_message.user.shadowban})\n" \
-                f"• <b>reason</b>: {user_message.user.banned_reason}\n" \
-                f"• <b>banned on</b>: {user_message.user.banned_on}"
+    if user.banned:
+        text += f"\n• <b>banned</b>: {user.banned} (shadowban: {user.shadowban})\n" \
+                f"• <b>reason</b>: {user.banned_reason}\n" \
+                f"• <b>banned on</b>: {user.banned_on}"
 
-    chat_member = chat_members.is_member(session, user_message.user.user_id, Chat.is_users_chat)
+    chat_member = chat_members.is_member(session, user.user_id, Chat.is_users_chat)
     if not chat_member:
         users_chat = chats.get_chat(session, Chat.is_users_chat)
         if users_chat:
-            chat_member_object = context.bot.get_chat_member(users_chat.chat_id, user_message.user.user_id)
+            chat_member_object = context.bot.get_chat_member(users_chat.chat_id, user.user_id)
             chat_member = DbChatMember.from_chat_member(users_chat.chat_id, chat_member_object)
             session.add(chat_member)
-            text += f"\n• <b>is member in users chat</b>: {chat_member.status_pretty()} (last update: {chat_member.updated_on})"
+            text += f"\n• <b>membership status in users chat</b>: {chat_member.status_pretty()} (last update: {chat_member.updated_on})"
     else:
-        text += f"\n• <b>is member in users chat</b>: {chat_member.status_pretty()} (last update: {chat_member.updated_on})"
+        text += f"\n• <b>membership status in users chat</b>: {chat_member.status_pretty()} (last update: {chat_member.updated_on})"
 
-    text += f"\n• #id{user_message.user.user_id}"
+    text += f"\n• #id{user.user_id}"
 
     await update.effective_message.reply_text(text)
 
 
 HANDLERS = (
-    (PrefixHandler(COMMAND_PREFIXES, 'info', on_info_command, ChatFilter.STAFF & filters.REPLY), Group.NORMAL),
+    (PrefixHandler(COMMAND_PREFIXES, 'info', on_info_command, ChatFilter.STAFF), Group.NORMAL),
 )
 
