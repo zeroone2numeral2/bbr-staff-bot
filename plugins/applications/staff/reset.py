d65b90df534faa8e4980a4a106b3a6ecdbb7c462
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-06-29 17:13:27 +0200

    /reset: modificata risposta pt. 2

--- plugins/applications/staff/reset.py
+++ plugins/applications/staff/reset.py
@@ -36,7 +36,7 @@ async def on_reset_command(update: Update, context: ContextTypes.DEFAULT_TYPE, s
 
     user.reset_evaluation()
     await update.message.reply_text(f"{user.mention()} ora potrà richiedere nuovamente di essere ammesso al gruppo "
-                                    f"(eventuali richieste pendenti o rifiutate sono state resettate)")
+                                    f"(eventuali richieste pendenti o rifiutate sono state dimenticate)")
 
     log_chat = chats.get_chat(session, Chat.is_log_chat)
     if not log_chat:
