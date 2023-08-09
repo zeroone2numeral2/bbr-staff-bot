2b160b8b12cc4f84e75b030cc25fa02e281eae5d
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-08-09 17:29:52 +0200

    spostata estrazione eventi in fuznione apposita

--- plugins/events/commands.py
+++ plugins/events/commands.py
@@ -212,18 +212,7 @@ async def on_events_command(update: Update, context: ContextTypes.DEFAULT_TYPE, 
     logger.info(f"/events {utilities.log(update)}")
 
     args = context.args if context.args else []
-    query_filters = extract_query_filters(args)
-
-    events_list: List[Event] = events.get_events(session, filters=query_filters)
-
-    all_events_strings = []
-    for i, event in enumerate(events_list):
-        if not event.is_valid():
-            logger.info(f"skipping invalid event: {event}")
-            continue
-
-        text_line = format_event_string(event)
-        all_events_strings.append(text_line)
+    all_events_strings = get_all_events_strings(session, args)
 
     # logger.debug(f"result: {len(messages_to_send)} messages, {len(text_lines)} lines")
 
@@ -358,6 +347,22 @@ def get_events_strings_from_cache(context: CallbackContext, args_cache_key: str)
     return context.bot_data[TempDataKey.EVENTS_CACHE][args_cache_key][TempDataKey.EVENTS_CACHE_DATA]
 
 
+def get_all_events_strings(session: Session, args: List[str]) -> List[str]:
+    query_filters = extract_query_filters(args)
+    events_list: List[Event] = events.get_events(session, filters=query_filters)
+
+    all_events_strings = []
+    for i, event in enumerate(events_list):
+        if not event.is_valid():
+            logger.info(f"skipping invalid event: {event}")
+            continue
+
+        text_line = format_event_string(event)
+        all_events_strings.append(text_line)
+
+    return all_events_strings
+
+
 @decorators.catch_exception()
 @decorators.pass_session()
 async def on_events_confirm_cb(update: Update, context: ContextTypes.DEFAULT_TYPE, session: Session):
@@ -369,17 +374,7 @@ async def on_events_confirm_cb(update: Update, context: ContextTypes.DEFAULT_TYP
 
     all_events_strings = get_events_strings_from_cache(context, args_cache_key)
     if not all_events_strings:
-        query_filters = extract_query_filters(args)
-        events_list: List[Event] = events.get_events(session, filters=query_filters)
-
-        all_events_strings = []
-        for i, event in enumerate(events_list):
-            if not event.is_valid():
-                logger.info(f"skipping invalid event: {event}")
-                continue
-
-            text_line = format_event_string(event)
-            all_events_strings.append(text_line)
+        all_events_strings = get_all_events_strings(session, args)
 
         logger.info(f"saving cache for key {args_cache_key}...")
         context.bot_data[TempDataKey.EVENTS_CACHE] = {
