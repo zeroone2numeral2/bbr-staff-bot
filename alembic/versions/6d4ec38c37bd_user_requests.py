362125222e253c6670d73fb7b9cd318a51a93311
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-08-10 12:11:42 +0200

    migrazioni db: ignora drop colonne falliti

--- alembic/versions/6d4ec38c37bd_user_requests.py
+++ alembic/versions/6d4ec38c37bd_user_requests.py
@@ -7,7 +7,7 @@ Create Date: 2023-05-10 14:21:50.360717
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.exc import OperationalError
 
 # revision identifiers, used by Alembic.
 revision = '6d4ec38c37bd'
@@ -16,11 +16,18 @@ branch_labels = None
 depends_on = None
 
 
+def drop_column_safe(table_name, column_name):
+    try:
+        op.drop_column(table_name, column_name)
+    except OperationalError:
+        pass
+
+
 def upgrade() -> None:
-    op.drop_column('users', 'application_id')
-    op.drop_column('users', 'application_status')
-    op.drop_column('users', 'application_received_on')
-    op.drop_column('users', 'application_evaluated_on')
+    drop_column_safe('users', 'application_id')
+    drop_column_safe('users', 'application_status')
+    drop_column_safe('users', 'application_received_on')
+    drop_column_safe('users', 'application_evaluated_on')
 
     # this returns an error: just leave the column there
     # op.drop_column('users', 'application_evaluated_by_user_id')
