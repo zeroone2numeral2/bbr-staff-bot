e31457ce1adb97c29e935941534bbf9dc18dc058
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-08-10 12:19:31 +0200

    migrazioni db: ignora errore nome colonna esistente

--- alembic/versions/4d76bf9221a4_staff_and_log_message_text.py
+++ alembic/versions/4d76bf9221a4_staff_and_log_message_text.py
@@ -7,6 +7,7 @@ Create Date: 2023-05-10 15:19:19.902963
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.exc import OperationalError
 
 
 # revision identifiers, used by Alembic.
@@ -16,9 +17,18 @@ branch_labels = None
 depends_on = None
 
 
+def add_colum_skip_duplicate_column_error(table_name, column):
+    try:
+        op.add_column(table_name, column)
+    except OperationalError as operational_error:
+        if "duplicate column name" in str(operational_error):
+            return
+        raise operational_error
+
+
 def upgrade() -> None:
-    op.add_column('application_requests', sa.Column('staff_message_text_html', sa.String))
-    op.add_column('application_requests', sa.Column('log_message_text_html', sa.String))
+    add_colum_skip_duplicate_column_error('application_requests', sa.Column('staff_message_text_html', sa.String))
+    add_colum_skip_duplicate_column_error('application_requests', sa.Column('log_message_text_html', sa.String))
 
 
 def downgrade() -> None:
