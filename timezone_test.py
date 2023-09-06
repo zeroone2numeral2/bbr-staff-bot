075fac233f3abe58af823c39cde575c2582bbf1d
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-09-06 10:31:59 +0200

    test timezones

--- timezone_test.py
+++ timezone_test.py
@@ -3,11 +3,13 @@ import pytz
 import sys
 
 print(f"python version: {sys.version}")
-print(f"pytz version: {pytz.__version__}")
+print(f"pytz version: {pytz.__version__}\n")
 
-utcnow = datetime.datetime.utcnow()
-now = utcnow.astimezone(pytz.timezone("Europe/Rome"))
+utc_now = datetime.datetime.now(datetime.timezone.utc)
+utc_now_naive = datetime.datetime.utcnow()
+now_tzaware = utc_now.astimezone(pytz.timezone("Europe/Rome"))
 
-print(f"utcnow: {utcnow}")
-print(f"now: {now}")
-print(f"utcnow.hour{utcnow.hour}, now.hour: {now.hour}")
+print(f"utc now: {utc_now}")
+print(f"utc now naive: {utc_now_naive}")
+print(f"now_tzaware: {now_tzaware}")
+print(f"utc_now_naive.hour: {utc_now_naive.hour}, utc_now.hour: {utc_now.hour}, now_tzaware.hour: {now_tzaware.hour}")
