9be598bcf25303ac24a761684baaa3817515c809
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-09-12 12:52:36 +0200

    migrazioni per #118 e #119

---
+++ alembic/versions/23304df82140_event_validity_notifications.py
@@ -0,0 +1,26 @@
+"""event validity notifications
+
+Revision ID: 23304df82140
+Revises: 5688af82d59a
+Create Date: 2023-09-12 12:38:48.153504
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23304df82140'
+down_revision = '5688af82d59a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('events', sa.Column('dates_from_hashtags', sa.Boolean))
+    op.add_column('events', sa.Column('send_validity_notifications', sa.Boolean))
+
+
+def downgrade() -> None:
+    op.drop_column('events', 'dates_from_hashtags')
+    op.drop_column('events', 'send_validity_notifications')
