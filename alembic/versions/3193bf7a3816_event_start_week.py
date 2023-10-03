9a73e4cefb229bc191e4d8a2683b2d3ad6d4a029
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-10-03 15:19:14 +0200

    start_week migration (#126)

---
+++ alembic/versions/3193bf7a3816_event_start_week.py
@@ -0,0 +1,25 @@
+"""event start week
+
+Revision ID: 3193bf7a3816
+Revises: a4d38d0ac10e
+Create Date: 2023-10-03 15:17:47.034965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3193bf7a3816'
+down_revision = 'a4d38d0ac10e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('events', sa.Column('start_week', sa.Integer))
+
+
+def downgrade() -> None:
+    op.drop_column('events', 'start_week')
+
