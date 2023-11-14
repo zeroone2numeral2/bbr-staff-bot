1784887876be2470dd04816f991260fb0a1a82d5
zeroone2numeral2 <zeroone2numeral2@pm.me>
2023-11-14 08:35:41 +0100

    sql

---
+++ database/sql/approved_users_not_members.sql
@@ -0,0 +1,26 @@
+select
+	ar.user_id,
+	cm.chat_id,
+	u.first_name,
+	u.last_name,
+	u.username,
+	--ar.status as request_status,
+	ar.accepted_message_message_id,
+	ar.invite_link,
+	cm.status as membership,
+	cm.has_been_member,
+	cm.updated_on
+from application_requests as ar
+	left join users as u
+		on ar.user_id = u.user_id
+	left join chat_members as cm
+		on ar.user_id = cm.user_id
+		-- left 
+		and (cm.status not in ('creator', 'administrator', 'member') or cm.status is null)
+where 1=1
+	and ar.status = 1
+	and ar.accepted_message_message_id is not null
+	and cm.chat_id = (select chat_id from chats where is_users_chat = 1)
+order by
+	cm.updated_on;
+	
