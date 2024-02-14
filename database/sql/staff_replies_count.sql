450fe79752fe18a374758ebac03b82201ed3b476
zeroone2numeral2 <zeroone2numeral2@pm.me>
2024-02-14 10:09:51 +0100

    aggiornata query

--- database/sql/staff_replies_count.sql
+++ database/sql/staff_replies_count.sql
@@ -2,6 +2,7 @@ select c.*, u.name
 from (
 	select staff_user_id as user_id, count(*) as msgs
 	from admin_messages
+	where (revoked = 0 or revoked is null)
 	group by user_id
 ) as c
 	left join users as u
