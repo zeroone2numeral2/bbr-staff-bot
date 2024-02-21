58241da1350d9adc92f8f91a38b5e7db141e4fb2
zeroone2numeral2 <zeroone2numeral2@pm.me>
2024-02-21 16:47:16 +0100

    inviat markup per approvazione tra i commenti
    se la si invia nel canale, telegram non mostra barra per accedere ai commenti

--- plugins/applications/users/request.py
+++ plugins/applications/users/request.py
@@ -92,18 +92,6 @@ def get_done_keyboard(input_field_placeholder: Optional[str] = None):
     )
 
 
-def get_evaluation_keyboard(user_id: int, application_id: int, include_reset=True):
-    keyboard = [[
-        InlineKeyboardButton(f"{Emoji.GREEN} accetta", callback_data=f"accept:{user_id}:{application_id}"),
-        InlineKeyboardButton(f"{Emoji.RED} rifiuta", callback_data=f"reject:{user_id}:{application_id}")
-    ]]
-    if include_reset:
-        reset_button = InlineKeyboardButton(f"{Emoji.RECYCLE} resetta", callback_data=f"reset:{user_id}:{application_id}")
-        keyboard[0].insert(1, reset_button)
-
-    return InlineKeyboardMarkup(keyboard)
-
-
 def get_text(session: Session, ltext_key: str, user: TelegramUser, raise_if_no_fallback: Optional[bool] = True) -> Optional[str]:
     fallback_language = settings.get_or_create(session, BotSettingKey.FALLBACK_LANGAUGE).value()
     ltext = texts.get_localized_text_with_fallback(
@@ -412,135 +400,6 @@ async def on_done_button(update: Update, context: ContextTypes.DEFAULT_TYPE, ses
     return State.WAITING_DESCRIBE_SELF
 
 
-async def send_application_to_staff_old(bot: Bot, evaluation_chat_id: int, log_chat_id: int, request: ApplicationRequest, user: TelegramUser):
-    # we will save the whole list of attachments we sent just in case we will need to link them,
-    # but we actually need just the first one (because it's the message we reply to)
-    sent_attachment_messages: List[Message] = []
-
-    messages_to_send_as_album: List[DescriptionMessage] = []
-    text_messages_to_merge: List[DescriptionMessage] = []
-    single_media_messages: List[DescriptionMessage] = []
-
-    # in this loop we populate the lists we initialized above
-    description_message: DescriptionMessage
-    for description_message in request.description_messages:
-        if description_message.is_social_message() or description_message.is_other_members_message():
-            # we include them in the log/staff message with the user's details
-            continue
-
-        if description_message.can_be_grouped():
-            messages_to_send_as_album.append(description_message)
-            continue
-
-        if description_message.text:
-            # sent_message = await bot.send_message(log_chat_id, description_message.text_html)
-            text_messages_to_merge.append(description_message)
-            continue
-
-        if description_message.type in (DescriptionMessageType.VOICE, DescriptionMessageType.VIDEO_MESSAGE):
-            single_media_messages.append(description_message)
-            continue
-
-        logger.warning(f"unexpected description message: {description_message}")
-
-    # no idea why but we *need* large timeouts
-    timeouts = dict(connect_timeout=300, read_timeout=300, write_timeout=300)
-
-    # merge and send all DescriptionMessage that contain the text the user sent as presentation
-    logger.debug("merging and sending presentation text messages...")
-    merged_text = f"{Emoji.SHEET} <b>presentazione</b> • #rid{request.id}"
-    merged_text_includes = []  # list of indexes of the DescriptionMessage that have been merged into the current merged_text
-    for i, description_message in enumerate(text_messages_to_merge):
-        if len(merged_text) + len(description_message.text_html) > MessageLimit.MAX_TEXT_LENGTH:
-            sent_message = await bot.send_message(log_chat_id, merged_text, **timeouts)
-            sent_attachment_messages.append(sent_message)
-            for j in merged_text_includes:
-                # save the message we just sent as the log message for each one of the DescriptionMessage that were merged into it
-                text_messages_to_merge[j].set_log_comment_message(sent_message)
-
-            merged_text = ""
-            merged_text_includes = []
-        else:
-            merged_text += f"\n\n{description_message.text_html}"
-            merged_text_includes.append(i)
-
-    # send what's left, if anything
-    if merged_text:
-        logger.debug("sending what's left...")
-        sent_message = await bot.send_message(log_chat_id, merged_text, **timeouts)
-        sent_attachment_messages.append(sent_message)
-        for i in merged_text_includes:
-            # save the message we just sent as the log message for each one of the DescriptionMessage that were merged into it
-            text_messages_to_merge[i].set_log_comment_message(sent_message)
-
-    # merge into an album and send all DescriptionMessage that can be grouped into an album
-    logger.debug("merging and sending presentation media messages that fit into albums...")
-    input_medias = []
-    for i, description_message in enumerate(messages_to_send_as_album):
-        input_medias.append(description_message.get_input_media())
-
-        if len(input_medias) == MediaGroupLimit.MAX_MEDIA_LENGTH:
-            logger.debug("album limit reached: sending media group...")
-            sent_messages = await bot.send_media_group(log_chat_id, media=input_medias, **timeouts)
-            sent_attachment_messages.append(sent_messages[0])  # we will link just the first one
-            # save sent log message
-            for j, sent_message in enumerate(sent_messages):
-                index = int(i / MediaGroupLimit.MAX_MEDIA_LENGTH) + j
-                logger.debug(f"saving log message with index {index}...")
-                messages_to_send_as_album[index].set_log_comment_message(sent_message)
-
-            input_medias = []
-
-    # send what's left, if anything
-    medias_count = len(input_medias)
-    if input_medias:
-        logger.debug(f"sending what's left ({input_medias})...")
-        sent_messages = await bot.send_media_group(log_chat_id, media=input_medias, **timeouts)
-        sent_attachment_messages.append(sent_messages[0])  # we will link just the first one
-        for i, sent_message in enumerate(sent_messages):
-            index = (medias_count - i) * -1  # go through the list from the last item
-            logger.debug(f"saving log message with index {index}...")
-            messages_to_send_as_album[index].set_log_comment_message(sent_message)
-
-    # send all DescriptionMessage that are a media and cannot be grouped
-    logger.debug("sending presentation text messages tthat should be sent on their own...")
-    for description_message in single_media_messages:
-        if description_message.type == DescriptionMessageType.VOICE:
-            sent_message = await bot.send_voice(log_chat_id, description_message.media_file_id, caption=description_message.caption_html)
-        elif description_message.type == DescriptionMessageType.VIDEO_MESSAGE:
-            sent_message = await bot.send_video_note(log_chat_id, description_message.media_file_id)
-        else:
-            continue
-
-        description_message.set_log_comment_message(sent_message)
-        sent_attachment_messages.append(sent_message)
-
-    # create and send the main log message with user info, social, other members, and links to attachments
-    user_mention = utilities.mention_escaped(user)
-    user_username = f"@{user.username}" if user.username else "username non impostato"
-    base_text = f"{Emoji.SPARKLE} <b>nuova #richiesta</b> • #rid{request.id} • #pendente • #nojoin\n\n" \
-                f"{Emoji.PERSON} <b>utente</b>\n" \
-                f"• {user_mention}\n" \
-                f"• {user_username}\n" \
-                f"• #id{user.id}"
-
-    other_members_text = utilities.escape_html(request.other_members_text or "non forniti")
-    base_text += f"\n\n{Emoji.PEOPLE} <b>utenti garanti</b>\n{other_members_text}"
-
-    social_text = utilities.escape_html(request.social_text or "non forniti")
-    base_text += f"\n\n{Emoji.PHONE} <b>social</b>\n{social_text}"
-
-    logger.debug("sending log message...")
-    log_message: Message = await sent_attachment_messages[0].reply_html(base_text, quote=True, **timeouts)
-    request.set_log_message(log_message)
-
-    logger.debug("sending staff message...")
-    staff_message_text = f"{base_text}\n\n{Emoji.SHEET} <b>presentazione ed allegati</b>\n<a href=\"{request.log_message_link()}\">vai al log</a>"
-    staff_message_reply_markup = get_evaluation_keyboard(request.user_id, request.id)
-    staff_message: Message = await bot.send_message(evaluation_chat_id, staff_message_text, reply_markup=staff_message_reply_markup, **timeouts)
-    request.set_staff_message(staff_message)
-
-
 async def send_application_log_message(bot: Bot, log_chat_id: int, request: ApplicationRequest, user: TelegramUser):
     # create and send the log message with user info, social, and other members
     user_mention = utilities.mention_escaped(user)
@@ -560,13 +419,10 @@ async def send_application_log_message(bot: Bot, log_chat_id: int, request: Appl
     # no idea why but we *need* large timeouts
     timeouts = dict(connect_timeout=300, read_timeout=300, write_timeout=300)
 
-    reply_markup = get_evaluation_keyboard(request.user_id, request.id)
-
     logger.debug("sending log message...")
     log_message: Message = await bot.send_message(
         chat_id=log_chat_id,
         text=base_text,
-        reply_markup=reply_markup,
         **timeouts
     )
     request.set_log_message(log_message)
