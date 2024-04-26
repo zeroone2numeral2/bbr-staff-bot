f3a5a8fd00031f9bddcd34e44cdc1562c08ae938
zeroone2numeral2 <zeroone2numeral2@pm.me>
2024-04-26 11:45:21 +0200

    prova ad unpinnare messaggi vecchie richieste non resettate (#202)

--- plugins/applications/staff/reset.py
+++ plugins/applications/staff/reset.py
@@ -66,6 +66,11 @@ async def mark_previous_requests_as_reset(bot: Bot, session: Session, user_id: i
             # add the #reset text only if the request is not marked as reset already
             reset_additional_text = get_reset_text(request.user, admin_user, add_user_info=False, add_explanation=False)
 
+            # try to unpin only if the request wasn't reset already
+            if request.staff_message_chat_id and request.staff_message_message_id:
+                logger.info("trying to unpin evaluation chat log message...")
+                await utilities.unpin_by_ids_safe(bot, request.staff_message_chat_id, request.staff_message_chat_id)
+
         logger.info(f"marking request {request.id} as reset")
         request.reset = True  # mark the request as reset as soon as possible
         requests_reset_count += 1
